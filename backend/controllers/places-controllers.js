b1e2b89e7feb7ef2d3e2db8e1f17305e79be3107
omar saadeh <omarsaadeh11@gmail.com>
2023-01-27 08:01:09 -0800

    using google's geocoding Api to Convert an Address to Coordinates

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -48,7 +48,9 @@ const createPlace = async (req, res, next) => {
     console.log(errors);
     // our error handling
     //deyman bas tkun bi async bta3mul next..mesh throw li2an throw synchro
-    next(new HttpError("Invalid inputs passed, please check your data.", 422));
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
   // const title = req.body.title;
   const { title, description, address, creator } = req.body;
@@ -64,7 +66,7 @@ const createPlace = async (req, res, next) => {
     id: uuid(),
     title,
     description,
-    location: getCoordsForAddress(),
+    location: coordinates,
     address,
     creator,
   };
@@ -120,3 +122,152 @@ exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
 // uui package : a package which is capable fo genearting unique IDs
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using google geocoding api
+
+const uuid = require("uuid/v4");
+const { validationResult } = require("express-validator");
+
+const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../util/location");
+
+let DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scarpers in the world!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.987156,
+    },
+    address: "20 w 34th ST,  New York , NY 10001",
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+  res.json({ place });
+};
+
+const getPlacesByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const places = DUMMY_PLACES.filter((p) => {
+    return p.creator === userId;
+  });
+  if (!places || places.length === 0) {
+    return next(
+      new HttpError("Could not find places for the provided user id.", 404)
+    );
+  }
+  res.json({ places });
+};
+
+const createPlace = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    // our error handling
+    //deyman bas tkun bi async bta3mul next..mesh throw li2an throw synchro
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
+  }
+  // const title = req.body.title;
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
+
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace); // unshift(createdPlace) if u want to add it as first element in the array
+  res.status(201).json({ place: createdPlace });
+};
+
+const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    // our error handling
+    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+  }
+
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id == placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+  // status code 200 not 201 bz we didnt create anything new
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+
+  if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
+    throw new HttpError("Could not find a place for that id.", 404);
+  }
+
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+  res.status(200).json({ message: "Deleted place." });
+
+  
+  //const placeId = req.params.pid;
+  //const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  //DUMMY_PLACES.splice(placeIndex,1);
+ 
+};
+
+
+// export single thing
+module.exports = getPlacesByUserId
+
+//exports multiple thing
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
+exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
+// uui package : a package which is capable fo genearting unique IDs
+
+
+
+
+*/
